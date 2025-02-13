b6e22f3c69995e4fb82fdfd8c0d11fcbe66a8e9e
shashank-1027 <shashankshettu631@gmail.com>
2025-02-13 17:21:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js as newer version
+// alert("hello") we are using node js not brower 
+
+let name = "shashank"  //string datatype
+let age = 22// int
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+// string => ""
+//bigInt => 
+    // boolean=> yes or no
+// null =>
+    //undefined => undefined
+// symbol
+//object
+
+console.log( typeof age);
