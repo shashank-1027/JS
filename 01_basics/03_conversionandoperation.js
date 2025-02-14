e5430586554ba05302d73668accf5b215c971f10
shashank-1027 <shashankshettu631@gmail.com>
2025-02-14 17:49:28 +0000

    comparision

--- 01_basics/03_conversionandoperation.js
+++ 01_basics/03_conversionandoperation.js
@@ -1,3 +1,31 @@
-let score = 33
+let score = undefined
 
-console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abs" => NaN
+//true => 1;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+//0 => false
+//"hitesh" => true
+//""=> false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *****************************operation **************************************//
+
