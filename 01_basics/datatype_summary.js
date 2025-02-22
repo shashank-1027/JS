55641942de50b7ae7de278b4e8d0cf5f5305ba00
shashank-1027 <shashankshettu631@gmail.com>
2025-02-22 13:33:28 +0000

    datatype

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -4,4 +4,29 @@
 //#refrence type or non primitive
 // array, object, function
 
-//
+//  *********************************************
+
+//stack(primitive)  call by value
+//heap(non primitive) call by reference
+
+let myYoutubeName = 'shetty.com' 
+let anotherName  = "myYoutubeName"
+anotherName = "sansha"
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = 
+{
+    email: "shetty@gmail.com",
+    upi:"user@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "sansha@gmail.com"
+console.log(userTwo.email);
+console.log(userOne.email);
+
+
+
